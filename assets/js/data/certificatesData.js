0803a222ac59e29dfbbd386cce84ca86df3267c5
sarpowsky <sarpcankaraman@gmail.com>
2025-09-10 02:00:58 +0300

    updated certificates

--- assets/js/data/certificatesData.js
+++ assets/js/data/certificatesData.js
@@ -3,11 +3,32 @@ export const certificatesData = {
     title: "Certificates & Achievements",
     certificates: [
         {
+            name: "HCCDA - Tech Essentials",
+            company: "Huawei Cloud",
+            date: "September 2025",
+            image: "../images/certificates/huawei-techessentials.png",
+            description: " Certified knowledge of Huawei's core technologies, including networking, cloud computing, storing and services."
+        },
+        {
+            name: "BTK Datathon 2025",
+            company: "BTK Akademi",
+            date: "September 2025",
+            image: "../images/certificates/btk-datathon2025.png",
+            description: " Datathon focused on an e-commerce dataset, involving data cleaning, exploratory data analysis, and predictive modeling to derive actionable insights. Out of 571 teams, we ranked 133th on the public score"
+        },
+        {
             name: "InternCamp2025",
             company: "TECHBROS",
             date: "August 2025",
             image: "../images/certificates/interncamp.png",
             description: "Comprehensive month-long training program covering enterprise systems, data analytics, and emerging technologies including ERP/CRM integration, DevOps, BI, and ML applications."
+        },
+        {
+            name: "BTK Hackathon 2025",
+            company: "BTK Akademi",
+            date: "August 2025",
+            image: "../images/certificates/btk-hackathon2025.png",
+            description: "Hackathon focused on developing innovative solutions using AI, cloud computing, and big data technologies to address real-world challenges."
         },
         {
             name: "SQL Mastery Workshop",
